2ecc405b76ad71a39a08480104843ece0a8cfee8
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-07-28 19:59:21 +0200

    loading iris data to tensorflow

--- iris.py
+++ iris.py
@@ -4,4 +4,11 @@ import tensorflow as tf
 
 iris_df = pd.read_csv('iris.data', header=None)
 
-print(iris_df.head())
+iris_df.columns = ['sepal_length', 'sepal_width', 'petal_width', 'petal_length', 'label']
+iris_df["label"] = iris_df["label"].map({'Iris-setosa':0, 'Iris-versicolor':1, 'Iris-virginica':2})
+
+iris_df = iris_df.sample(frac=1.0, random_state=4321)
+x = iris_df[["sepal_length", "sepal_width", "petal_width", "petal_length"]]
+x = x - x.mean(axis=0)
+y = tf.one_hot(iris_df["label"], depth=3)
+print(y)

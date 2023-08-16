5a2932dfcac83f8a5d2f9751e5506e8adf57793a
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-08-16 20:13:30 +0200

    moving the code to a note book

--- iris.py
+++ iris.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import tensorflow as tf
+from pandas import DataFrame, Series
 
-
-iris_df = pd.read_csv('iris.data', header=None)
-
+iris_df: DataFrame = pd.read_csv('iris.data', header=None)
 iris_df.columns = ['sepal_length', 'sepal_width', 'petal_width', 'petal_length', 'label']
 iris_df["label"] = iris_df["label"].map({'Iris-setosa':0, 'Iris-versicolor':1, 'Iris-virginica':2})
+labes: Series = iris_df["label"]
+iris_df: DataFrame = iris_df.sample(frac=1.0, random_state=4321)
 
-iris_df = iris_df.sample(frac=1.0, random_state=4321)
-x = iris_df[["sepal_length", "sepal_width", "petal_width", "petal_length"]]
+
+x: DataFrame = iris_df[["sepal_length", "sepal_width", "petal_width", "petal_length"]]
 x = x - x.mean(axis=0)
 y = tf.one_hot(iris_df["label"], depth=3)
-print(y)
 
 
 from tensorflow.keras.layers import Dense
@@ -25,3 +25,5 @@ model = Sequential([
     Dense(3, activation='softmax')
 ])
 
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.summary()
